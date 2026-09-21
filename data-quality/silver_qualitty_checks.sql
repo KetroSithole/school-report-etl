5f2a2c7cf88c5b238eecac5af807e12b88f007ee
greek-hacker <u18312889@tuks.co.za>
2026-09-21 19:31:23 +0200

    Add data quality checks and queries for silver layer student marks

---
+++ data-quality/silver_qualitty_checks.sql
@@ -0,0 +1,3 @@
+select * from [sa_college_dwh].[silver].[prelim_science_students_marks_g10]
+select * from [sa_college_dwh].[silver].[prelim_science_students_marks_g11]
+select * from [sa_college_dwh].[silver].[prelim_science_students_marks_g12]
