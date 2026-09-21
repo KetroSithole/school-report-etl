0d85f3938ab594a5dac92964adeafe8fb9115975
greek-hacker <u18312889@tuks.co.za>
2026-09-21 19:50:59 +0200

    Refactor silver layer table creation and insert logic for grade 10, 11, and 12 science student marks

--- 003_create_silver_tables copy.sql
+++ 003_create_silver_tables copy.sql
@@ -1,19 +1,19 @@
-USE sa_college_dwh;
+TRUNCATE TABLE sa_college_dwh.silver.prelim_science_students_marks_g10;
+TRUNCATE TABLE sa_college_dwh.silver.prelim_science_students_marks_g11;
+TRUNCATE TABLE sa_college_dwh.silver.prelim_science_students_marks_g12;
 
+USE sa_college_dwh;
 GO
-    ------------------------------------------------------------
-    -- GRADE 10
-    ------------------------------------------------------------
-    IF NOT EXISTS (
-        SELECT
-            1
-        FROM
-            sys.tables t
-            JOIN sys.schemas s ON t.schema_id = s.schema_id
-        WHERE
-            s.name = 'silver'
-            AND t.name = 'prelim_science_students_marks_g10'
-    ) BEGIN CREATE TABLE silver.prelim_science_students_marks_g10 (
+
+------------------------------------------------------------
+-- GRADE 10
+------------------------------------------------------------
+IF NOT EXISTS (
+    SELECT 1 FROM sys.tables t
+    JOIN sys.schemas s ON t.schema_id = s.schema_id
+    WHERE s.name = 'silver' AND t.name = 'prelim_science_students_marks_g10'
+) BEGIN
+    CREATE TABLE silver.prelim_science_students_marks_g10 (
         student_id NVARCHAR(50),
         student_name NVARCHAR(200),
         grade NVARCHAR(10),
@@ -27,43 +27,43 @@ GO
         total_mark DECIMAL(6, 2),
         average_mark DECIMAL(5, 2)
     );
-
 END
 GO
+
 INSERT INTO
     silver.prelim_science_students_marks_g10
 SELECT
-    [student_id],
-    [student_name],
-    [grade],
-    [mathematics_mark],
-    [physical_science_mark],
-    [life_sciences_mark],
-    [english_home_language_mark],
-    [life_orientation_mark],
-    [information_technology_mark],
-    [agricultural_science_mark],
-    [total_mark],
-    [average_mark]
+    A.[student_id],
+    A.[student_name],
+    A.[grade],
+    A.[mathematics_mark],
+    A.[physical_science_mark],
+    A.[life_sciences_mark],
+    A.[english_home_language_mark],
+    A.[life_orientation_mark],
+    A.[information_technology_mark],
+    A.[agricultural_science_mark],
+    A.[total_mark],
+    A.[average_mark]
 FROM
-    [sa_college_stg].[bronze].[prelim_science_students_marks]
-WHERE
-    grade IN ('10A', '10B');
-
+    [sa_college_stg].[bronze].[prelim_science_students_marks] AS A
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM [sa_college_dwh].[silver].[prelim_science_students_marks_g10] AS B
+    WHERE A.student_id = B.student_id
+)
+AND grade IN ('10A', '10B');
 GO
-    ------------------------------------------------------------
-    -- GRADE 11
-    ------------------------------------------------------------
-    IF NOT EXISTS (
-        SELECT
-            1
-        FROM
-            sys.tables t
-            JOIN sys.schemas s ON t.schema_id = s.schema_id
-        WHERE
-            s.name = 'silver'
-            AND t.name = 'prelim_science_students_marks_g11'
-    ) BEGIN CREATE TABLE silver.prelim_science_students_marks_g11 (
+
+------------------------------------------------------------
+-- GRADE 11
+------------------------------------------------------------
+IF NOT EXISTS (
+    SELECT 1 FROM sys.tables t
+    JOIN sys.schemas s ON t.schema_id = s.schema_id
+    WHERE s.name = 'silver' AND t.name = 'prelim_science_students_marks_g11'
+) BEGIN
+    CREATE TABLE silver.prelim_science_students_marks_g11 (
         student_id NVARCHAR(50),
         student_name NVARCHAR(200),
         grade NVARCHAR(10),
@@ -77,43 +77,43 @@ GO
         total_mark DECIMAL(6, 2),
         average_mark DECIMAL(5, 2)
     );
-
 END
 GO
+
 INSERT INTO
     silver.prelim_science_students_marks_g11
 SELECT
-    [student_id],
-    [student_name],
-    [grade],
-    [mathematics_mark],
-    [physical_science_mark],
-    [life_sciences_mark],
-    [english_home_language_mark],
-    [life_orientation_mark],
-    [information_technology_mark],
-    [agricultural_science_mark],
-    [total_mark],
-    [average_mark]
+    A.[student_id],
+    A.[student_name],
+    A.[grade],
+    A.[mathematics_mark],
+    A.[physical_science_mark],
+    A.[life_sciences_mark],
+    A.[english_home_language_mark],
+    A.[life_orientation_mark],
+    A.[information_technology_mark],
+    A.[agricultural_science_mark],
+    A.[total_mark],
+    A.[average_mark]
 FROM
-    [sa_college_stg].[bronze].[prelim_science_students_marks]
-WHERE
-    grade IN ('11A', '11B');
-
+    [sa_college_stg].[bronze].[prelim_science_students_marks] AS A
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM [sa_college_dwh].[silver].[prelim_science_students_marks_g11] AS B
+    WHERE A.student_id = B.student_id
+)
+AND grade IN ('11A', '11B');
 GO
-    ------------------------------------------------------------
-    -- GRADE 12
-    ------------------------------------------------------------
-    IF NOT EXISTS (
-        SELECT
-            1
-        FROM
-            sys.tables t
-            JOIN sys.schemas s ON t.schema_id = s.schema_id
-        WHERE
-            s.name = 'silver'
-            AND t.name = 'prelim_science_students_marks_g12'
-    ) BEGIN CREATE TABLE silver.prelim_science_students_marks_g12 (
+
+------------------------------------------------------------
+-- GRADE 12
+------------------------------------------------------------
+IF NOT EXISTS (
+    SELECT 1 FROM sys.tables t
+    JOIN sys.schemas s ON t.schema_id = s.schema_id
+    WHERE s.name = 'silver' AND t.name = 'prelim_science_students_marks_g12'
+) BEGIN
+    CREATE TABLE silver.prelim_science_students_marks_g12 (
         student_id NVARCHAR(50),
         student_name NVARCHAR(200),
         grade NVARCHAR(10),
@@ -127,27 +127,30 @@ GO
         total_mark DECIMAL(6, 2),
         average_mark DECIMAL(5, 2)
     );
-
 END
 GO
+
 INSERT INTO
     silver.prelim_science_students_marks_g12
 SELECT
-    [student_id],
-    [student_name],
-    [grade],
-    [mathematics_mark],
-    [physical_science_mark],
-    [life_sciences_mark],
-    [english_home_language_mark],
-    [life_orientation_mark],
-    [information_technology_mark],
-    [agricultural_science_mark],
-    [total_mark],
-    [average_mark]
+    A.[student_id],
+    A.[student_name],
+    A.[grade],
+    A.[mathematics_mark],
+    A.[physical_science_mark],
+    A.[life_sciences_mark],
+    A.[english_home_language_mark],
+    A.[life_orientation_mark],
+    A.[information_technology_mark],
+    A.[agricultural_science_mark],
+    A.[total_mark],
+    A.[average_mark]
 FROM
-    [sa_college_stg].[bronze].[prelim_science_students_marks]
-WHERE
-    grade IN ('12A', '12B');
-
+    [sa_college_stg].[bronze].[prelim_science_students_marks] AS A
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM [sa_college_dwh].[silver].[prelim_science_students_marks_g12] AS B
+    WHERE A.student_id = B.student_id
+)
+AND grade IN ('12A', '12B');
 GO
